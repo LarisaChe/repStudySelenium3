c3c12cef59e1c898a50a0ec2284f20c6c39aad07
LarisaChe <lchernaya@masterdm.ru>
2017-04-23 11:21:25 +0300

    Задание 1

---
+++ HW01_Test/src/test/java/Test01.java
@@ -0,0 +1,40 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class Test01 {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeTest
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 100);
+    }
+
+    @Test
+    public void trytest() {
+        driver.get("http://www.google.com");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    //@AfterTest
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
+
